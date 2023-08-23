eff003fffc7cab0828d272563cc31fdce5ad1184
Marcelino Teixeira <marcelino.teixeira.dev@gmail.com>
2023-08-23 17:27:58 -0300

    feat: add heading component

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Elemento Título',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'Título h1',
+    as: 'h1',
+  },
+}
